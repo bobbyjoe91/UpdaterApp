f519594f6681b2f555327a8a19aa89a1ef2fa558
bobbyjoe91 <bobby.cool00763@gmail.com>
2021-04-17 18:52:41 +0700

    adding decimal points in numbers

--- src/components/StatsCard.js
+++ src/components/StatsCard.js
@@ -1,5 +1,26 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { add } from 'react-native-reanimated';
+
+function addDecimalPoints(numString) {
+    if(numString) {
+        let numStrArray = numString.split('');
+        numStrArray.reverse();
+
+        let length = numString.length;
+
+        if(length > 3) {
+            let index = (length%3 == 0) && (length > 3) ? Math.floor(length / 3)-1 : Math.floor(length / 3);
+            for(let i=1; i <= index; i++) {
+                numStrArray.splice(4*i - 1, 0, '.');
+            }
+            
+            return numStrArray.reverse().join('');
+        }
+        
+        return numString;
+    }
+}
 
 function StatsCard(props) {
     const styles = StyleSheet.create({
@@ -23,7 +44,9 @@ function StatsCard(props) {
     return(
         <View style={styles.container}>
             <Text style={{...styles.p, ...styles.title, fontSize: 18}}>{props.title}</Text>
-            <Text style={{...styles.p, fontSize: 36}}>{props.p}</Text>
+            <Text style={{...styles.p, fontSize: 36}}>
+                {addDecimalPoints(String(props.p))}
+            </Text>
         </View>
     );
 }
